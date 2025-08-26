a7485d6f6b05fdcd2a781d7da146c51333310a79
0701yuma2025-commits <0701yuma2025@gmail.com>
2025-08-26 11:35:01 +0900

    Update next.config.js
    型チェックを無
      効化：
    
      /** @type {import('next').NextConfig} */
      const nextConfig = {
        reactStrictMode: true,
        swcMinify: true,
        typescript: {
          ignoreBuildErrors: true,
        },
        eslint: {
          ignoreDuringBuilds: true,
        },
      }
    
      module.exports = nextConfig

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+  const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+  }
 
-module.exports = nextConfig
+  module.exports = nextConfig
